a031f170bccf72d171c6fa68d6ee15aec28bae35
Matthieu - Greep <42576124+GreepTheSheep@users.noreply.github.com>
2021-01-18 13:16:07 +0100

    First alpha version (#3)
    * Init loading Window and main Window
    
    * 🆙Update eslint to version 7.18.0 (#2)
    
    Co-authored-by: depfu[bot] <23717796+depfu[bot]@users.noreply.github.com>
    
    * Add dev mode on app
    
    * add auto-updater on loading Window
    
    * Fix updater
    
    * Change electron version (v11 will not start)
    
    * Add badges
    
    * updater patches
    
    * Add custom titlebar to main Window
    
    * Init main page
    
    * Update preload.js
    
    Co-authored-by: depfu[bot] <23717796+depfu[bot]@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
 # The Arendelle Odyssey desktop app
 
+![GitHub package.json version](https://img.shields.io/github/package-json/v/arendelleodyssey/desktop-app?color=purple&style=for-the-badge)
+![GitHub package.json dependency version (dev dep on branch)](https://img.shields.io/github/package-json/dependency-version/ArendelleOdyssey/desktop-app/dev/electron?logo=electron&logoColor=white&style=for-the-badge)
+![GitHub release (latest by date)](https://img.shields.io/github/downloads/ArendelleOdyssey/desktop-app/latest/total?logo=github&style=for-the-badge)
+
+[![GitHub issues](https://img.shields.io/github/issues/ArendelleOdyssey/desktop-app?logo=github)](https://github.com/ArendelleOdyssey/desktop-app/issues)
+[![GitHub forks](https://img.shields.io/github/forks/ArendelleOdyssey/desktop-app?logo=github)](https://github.com/ArendelleOdyssey/desktop-app/network)
+[![GitHub stars](https://img.shields.io/github/stars/ArendelleOdyssey/desktop-app?logo=github)](https://github.com/ArendelleOdyssey/desktop-app/stargazers)
+
+[![Build Status](https://travis-ci.com/ArendelleOdyssey/desktop-app.svg?branch=main)](https://travis-ci.com/ArendelleOdyssey/desktop-app)
+[![DeepScan grade](https://deepscan.io/api/teams/6175/projects/15570/branches/312937/badge/grade.svg)](https://deepscan.io/dashboard#view=project&tid=6175&pid=15570&bid=312937)
 [![Depfu](https://badges.depfu.com/badges/3eeda04dce8e840ac0f6fd2df351f7d2/count.svg)](https://depfu.com/github/ArendelleOdyssey/desktop-app?project_id=21228)
 [![FOSSA Status](https://app.fossa.com/api/projects/git%2Bgithub.com%2FArendelleOdyssey%2Fdesktop-app.svg?type=shield)](https://app.fossa.com/projects/git%2Bgithub.com%2FArendelleOdyssey%2Fdesktop-app?ref=badge_shield)
-[![Build Status](https://travis-ci.com/ArendelleOdyssey/desktop-app.svg?branch=main)](https://travis-ci.com/ArendelleOdyssey/desktop-app)
 
 ---
 
---
+++ src/index.js
@@ -0,0 +1,140 @@
+const { autoUpdater } = require("electron-updater")
+const { app, BrowserWindow, ipcMain } = require('electron')
+const path = require('path')
+const execArgs = process.argv;
+var closeLoadWindow
+var loadWindow
+
+function createMainWindow () {
+  // Create the browser window.
+    const mainWindow = new BrowserWindow({
+      show : false,
+      //backgroundColor: '#000F42',
+      icon: 'build/icon.png',
+      title: 'Arendelle Odyssey',
+      frame: process.platform == 'darwin',
+      titleBarStyle: "hidden",
+      webPreferences: {
+        enableRemoteModule: true,
+        preload: path.join(__dirname, 'content', 'mainWindow', 'preload.js'),
+        nodeIntegration: true,
+      }
+    })
+    
+
+    mainWindow.setMenu(null);
+    
+    mainWindow.flashFrame(true)
+    mainWindow.once('focus', () => mainWindow.flashFrame(false))
+
+    // and load the index.html of the app.
+    //mainWindow.loadFile('content/mainWindow/index.html')
+    mainWindow.loadURL(`file://${__dirname}/content/mainWindow/index.html`)
+
+    //if (!window.isMaximized()) window.maximize()
+
+  // Open the DevTools.
+  //mainWindow.webContents.openDevTools()
+
+  mainWindow.on('ready-to-show', () => {
+    if (!mainWindow.isMaximized()) mainWindow.maximize()
+    mainWindow.show()
+    if (loadWindow != null) loadWindow.close();
+  })
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+//app.whenReady().then(createWindow)
+var resolved
+app.on('ready', async () => {
+  if (execArgs.includes('--develop') || execArgs.includes('-d')) {
+    console.log('Started in develop mode (Updater will not start)')
+    return createMainWindow();
+  } else {
+    resolved = false
+    loadWindow = new BrowserWindow({
+      width: 400,
+      height: 500,
+      webPreferences: {
+        enableRemoteModule: true,
+        preload: path.join(__dirname, 'content', 'loadWindow', 'preload.js'),
+        nodeIntegration: false,
+      },
+      transparent: false,
+      backgroundColor: '#252525',
+      icon: 'build/icon.png',
+      title: 'Loading Arendelle Odyssey',
+      frame: false,
+      center: true,
+      show: false
+    });
+    loadWindow.loadURL(`file://${__dirname}/content/loadWindow/index.html`)
+    loadWindow.setAlwaysOnTop(true); 
+    loadWindow.once('ready-to-show', () => {
+      loadWindow.show();
+    });
+    var checkMaximize = setInterval(() => {
+      if (loadWindow) loadWindow.unmaximize()
+    }, 0)
+    closeLoadWindow = () => {
+      clearInterval(checkMaximize)
+      loadWindow.close();
+    };
+
+    //loadWindow.webContents.openDevTools()
+    //await wait(5000)
+
+    try{
+      if (!autoUpdater.app.isPackaged) {
+        resolved = true
+        createMainWindow();
+      } else {
+        autoUpdater.checkForUpdates();
+      }
+    } catch (err) {
+      console.error(err)
+      resolved = true
+      createMainWindow();
+    }
+    
+
+    loadWindow.once('close', () =>{
+      loadWindow = null
+      if (resolved == false) {
+        app.quit()
+        process.exit(0)
+      }
+    })
+
+    ipcMain.on('closeLoad', () => {
+      if (loadWindow != null) closeLoadWindow()
+    })
+  }
+});
+
+ipcMain.on('online', () => {
+  resolved = true
+  createMainWindow();
+})
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createMainWindow()
+    }
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
